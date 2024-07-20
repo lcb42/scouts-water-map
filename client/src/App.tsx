f33e9790d4d8684c99a0a7c2d5acc311e6c0de71
lcb42 <lucy_blatherwick@autovia.co.uk>
2024-07-20 18:28:35 +0100

    SWM: Added database connection and test api route to get data to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [dbresult, setDbresult] = useState('Loading...');
+
+  async function getTestDBContents() {
+    try {
+      const response = await axios.get('/testdb');
+      console.warn('RESP: ', response.data);
+      setDbresult(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  getTestDBContents();
+
   return (
     <div className="App">
       <h1>This is an app with a map</h1>
+      <h1>{JSON.stringify(dbresult)}</h1>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const db = require('./db');
 
 //console.log that server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'});
-})
+app.get('/testdb', async (req, res) => {
+  console.log('testdb');
+  try {
+    const result = await db.query('SELECT * FROM test');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
