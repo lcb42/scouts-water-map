1dc18753eebe6e961fd45e69363ad33615d494e3
lcb42 <lucy.blatherwick@carwow.co.uk>
2025-03-26 21:40:30 +0000

    Updated test json data to actual test data in own file, and converted geojson component to use style classes rather than inline css

--- client/src/Components/GeoJsonTestElement/GeoJsonTestElement.tsx
+++ client/src/Components/GeoJsonTestElement/GeoJsonTestElement.tsx
@@ -3,22 +3,7 @@ import React from 'react';
 import './style.scss';
 import { GeoJson, PigeonProps } from 'pigeon-maps';
 import { Feature } from 'geojson';
-
-const geoJsonSample = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "geometry": {
-          "type": "Point",
-          "coordinates": [2.0, 48.5]
-        },
-      "properties": {
-          "width": "1"
-        }
-    }
-  ]
-};
+import { testData } from './geoJsonTestData';
 
 const GeoJsonTestElement = (
   props: PigeonProps
@@ -26,16 +11,13 @@ const GeoJsonTestElement = (
   return (
     <GeoJson
       {...props}
-      data={geoJsonSample}
+      data={testData}
       styleCallback={(feature: Feature) => {
         if (feature.geometry.type === "LineString") {
-          return { strokeWidth: "1", stroke: "black" };
+          return { strokeWidth: "1", stroke: "green" };
         }
         return {
-          fill: "#d4e6ec99",
-          strokeWidth: "1",
-          stroke: "white",
-          r: "20",
+          className: 'geo-json-polygon',
         };
       }}
     />
